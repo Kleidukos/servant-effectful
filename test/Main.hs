3c09f3567234db6f87aa039a1297578bddae99ab
Hécate Moonlight <hecate+github@glitchbra.in>
2022-07-16 00:07:54 +0200

    Write tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,20 @@
 {-# LANGUAGE NoOverloadedStrings #-}
 module Main where
 
+import Effectful ()
 import Prelude hiding (lookup)
 import Test.Tasty
+import Test.Tasty.HUnit
+import qualified Utils as U
+import Servant
+import Data.Text (Text)
+
+import Effectful.Servant
+
+type Api1 = "api" :> "v1" :> "name" :> Get '[JSON] Text
+
+server1 :: 
 
 main :: IO ()
-main = defaultMain $ testGroup "servant-effectful" [ ]
+main = defaultMain $ testGroup "servant-effectful"
+  [ ]
