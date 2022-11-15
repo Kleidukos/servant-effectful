0738067222d294a35ba7848a536c9135257d119d
Tristan Cacqueray <tdecacqu@redhat.com>
2022-11-15 12:31:31 +0000

    Update the API so that error effect is only part of server monad
    Fixes #5
    Fixes #6

--- src/Effectful/Servant.hs
+++ src/Effectful/Servant.hs
@@ -1,86 +1,63 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE RankNTypes #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
+-- Allow redendudant constraints to require IOE for runWarp helpers.
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
 
-module Effectful.Servant where
+module Effectful.Servant
+  ( -- * main api
+    runWarpServerSettings
+  , runWarpServerSettingsContext
+
+    -- * helpers
+  , serveEff
+  , interpretServer
+  )
+where
 
 import qualified Control.Monad.Except as T
 import Data.Kind (Type)
-import Data.Proxy (Proxy (..))
 import Effectful
 import Effectful.Dispatch.Static
+import Effectful.Dispatch.Static.Primitive (Env, cloneEnv)
 import Effectful.Error.Static
 import qualified Network.Wai.Handler.Warp as Warp
-import Servant.API.Generic (AsApi, GenericServant, ToServant, ToServantApi)
-import Servant.Server (Application, Context, Handler, HasServer, Server, ServerContext, ServerError, ServerT, hoistServer)
-import qualified Servant.Server as Servant
-import Servant.Server.Generic (AsServerT)
+import Servant hiding ((:>))
 
-type ServerEff api es = ServerT api (Eff es)
-
+-- | Deploy an effectful server.
 runWarpServerSettings
   :: forall (api :: Type) (es :: [Effect])
-   . (HasServer api '[], IOE :> es, Error ServerError :> es)
+   . (HasServer api '[], IOE :> es)
   => Warp.Settings
-  -> ServerEff api es
+  -> ServerT api (Eff (Error ServerError : es))
   -> Eff es ()
-runWarpServerSettings settings server = do
-  withEffToIO $ \runInIO -> do
-    let api = Proxy @api
-        server' = Servant.hoistServer @api api (effToHandlerWith runInIO) server
-    Warp.runSettings settings (Servant.serve api server')
+runWarpServerSettings settings =
+  runWarpServerSettingsContext @api settings EmptyContext
 
+-- | Deploy an effectful server with a context.
+runWarpServerSettingsContext
+  :: forall (api :: Type) (context :: [Type]) (es :: [Effect])
+   . (HasServer api context, ServerContext context, IOE :> es)
+  => Warp.Settings
+  -> Context context
+  -> ServerT api (Eff (Error ServerError : es))
+  -> Eff es ()
+runWarpServerSettingsContext settings ctx server = do
+  unsafeEff $ \es -> do
+    Warp.runSettings settings (serveEff @api es ctx server)
+
+-- | Convert an effectful server into a wai application.
 serveEff
-  :: forall (api :: Type) (es :: [Effect])
-   . (HasServer api '[], Error ServerError :> es)
-  => (forall x. Eff es x -> IO x)
-  -> ServerEff api es
+  :: forall (api :: Type) (context :: [Type]) (es :: [Effect])
+   . (HasServer api context, ServerContext context)
+  => Env es
+  -> Context context
+  -> ServerT api (Eff (Error ServerError : es))
   -> Application
-serveEff runInIO server = do
-  let api = Proxy @api
-  Servant.serve api (hoistServerEff api runInIO server)
+serveEff env ctx = Servant.serveWithContextT (Proxy @api) ctx (interpretServer env)
 
-hoistServerEff
-  :: (HasServer api '[], Error ServerError :> es)
-  => Proxy api
-  -> (forall x. Eff es x -> IO x)
-  -> ServerEff api es
-  -> Server api
-hoistServerEff api runInIO = Servant.hoistServer api (effToHandlerWith runInIO)
-
-effToHandler
-  :: forall (a :: Type)
-   . ()
-  => Eff '[Error ServerError, IOE] a
-  -> Handler a
-effToHandler computation = do
-  v <- liftIO . runEff . runErrorNoCallStack @ServerError $ computation
-  either T.throwError pure v
-
-effToHandlerWith
-  :: Error ServerError :> es
-  => (forall x. Eff es x -> IO x)
-  -> Eff es a
-  -> Handler a
-effToHandlerWith runInIO =
-  Servant.Handler
-    . T.withExceptT snd
-    . T.ExceptT
-    . runInIO
-    . tryError
-
-hoistServerIntoEff
-  :: forall (es :: [Effect]) (api :: Type)
-   . (HasServer api '[], Error ServerError :> es)
-  => ServerT api Handler
-  -> ServerT api (Eff es)
-hoistServerIntoEff = hoistServer (Proxy @api) (handlerToEff @es)
-
-handlerToEff
-  :: forall (es :: [Effect]) (a :: Type)
-   . (Error ServerError :> es)
-  => Handler a
-  -> Eff es a
-handlerToEff handler = do
-  v <- unsafeEff_ $ Servant.runHandler handler
-  either throwError pure v
+-- | Transform the Eff monad into a servant Handler.
+interpretServer :: Env es -> Eff (Error ServerError : es) a -> Servant.Handler a
+interpretServer env action = do
+  v <- liftIO $ do
+    es' <- cloneEnv env
+    unEff (runErrorNoCallStack action) es'
+  T.liftEither v
